33bf99ad17c6243922c8d2dd133a46f133d12725
Shtuffy <joshoplano@gmail.com>
2017-10-27 15:32:21 -0500

    Create CONSTS.java

---
+++ CONSTS.java
@@ -0,0 +1,28 @@
+package engine;
+
+
+/**
+* Collected constants of general utility.
+*
+* All members of this class are immutable. 
+*/
+public class CONSTS {
+	
+	//public constants 
+	public static final String SYMBOL_PREFIX = "_Var";
+	
+	public static final String ERROR = "Invalid input";
+	
+	public static final char ONE = '1';
+	
+	public static final char ZERO = '0';
+	
+	public static final int FAILURE = -1;
+	
+	public static final int UNDETERMINED = 1;
+	
+	public static final int SUCCESS = 1;
+	
+	//private constants
+
+}
