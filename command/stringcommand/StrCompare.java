f02e9cbd969fc533b037579703592815bf3b0b20
Shtuffy <joshoplano@gmail.com>
2017-10-27 15:50:57 -0500

    Create StrCompare.java

---
+++ command/stringcommand/StrCompare.java
@@ -0,0 +1,23 @@
+package engine.command.stringcommand;
+
+import engine.command.CommandExpression;
+import engine.command.CommandResponse;
+
+/**
+ * 
+ * @author Logan LaFollette
+ *
+ */
+public class StrCompare extends StrCmd {
+
+	public StrCompare() {
+		
+	}
+
+	@Override
+	public CommandResponse execute(CommandExpression cmd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
