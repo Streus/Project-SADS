f540f0a5e814e570eaf891f6ffecc10b4d2b8f4f
loganlafollette <loganlafollette@192.168.1.4>
2017-10-29 18:52:54 -0500

    added stack execution method in command executor

--- src/engine/command/commandExecutor.java
+++ src/engine/command/commandExecutor.java
@@ -1,7 +1,25 @@
 package engine.command;
 
-public class commandExecutor {
+import java.util.Stack;
+import grammar.CommandObject;
+import gui.Console;
 
+public class commandExecutor {
 	
+	//defualt
+	public commandExecutor() {
+		
+	}
+
+	public static void executeStack(Stack<CommandObject> cmdObj) {
+		
+		Console.instance();
+		
+		CommandObject obj = cmdObj.pop();
+		
+		CommandResponse resp = obj.execute();
+		
+		Console.print("Response from commandExecutor: " + resp.returnVal);
+	}
 
 }
