793569fe8cb3bd6eb6381c14c98fe5764d139b11
Shtuffy <joshoplano@gmail.com>
2017-11-20 13:03:00 -0600

    Update WordCount.java

--- src/engine/PredefinedFunctions/WordCount.java
+++ src/engine/PredefinedFunctions/WordCount.java
@@ -11,15 +11,37 @@ import java.util.*;
 
 public class WordCount
 {
-  public Map<String, Integer> wordCount(String word, int subWordLength) 
-  {
+	public static <String, Integer extends Comparable<Integer>> Map<String, Integer> sort(final Map<String, Integer> map) 
+	{
+	    	Comparator<String> comparator =  new Comparator<String>() 
+	    	{
+	        	public int compare(String value1, String value2) 
+	        	{
+	            		int compare = map.get(value2).compareTo(map.get(value1));
+	            		if(compare == 0)
+	            		{
+	            			return 1;
+	           		}
+	            		else
+	            		{
+	            			return compare;
+	            		}
+	       		}
+	 	};
+	    	Map<String, Integer> sorted = new TreeMap<String, Integer>(comparator);
+	    	sorted.putAll(map);
+	    	return new LinkedHashMap<String, Integer>(sorted);
+	}
+	
+  	public Map<String, Integer> wordCount(String word, int subWordLength) 
+  	{
 		Map<String, Integer> map = new HashMap<String, Integer>();
 		String subWord = "";
 		int newStartingIndex = 0;
 		int newEndingIndex = subWordLength;
 		
-        //Goes through the word and finds each subword starting at the beginning index 
-        //and ending at the subWordLength, then realigns the indices for the next subwords
+        	//Goes through the word and finds each subword starting at the beginning index 
+        	//and ending at the subWordLength, then realigns the indices for the next subwords
 		for (int i = 0; i < word.length(); i++)
 		{
 			subWord = word.substring(newStartingIndex, newEndingIndex);
@@ -42,8 +64,7 @@ public class WordCount
 			{
 				break;
 			}
-		}
-		
-		return map;
+		}		
+		return sort(map);
 	}
 }  
