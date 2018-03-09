8ebbdf1abdb3e75326462ed34d7957c25aa5d991
Shtuffy <joshoplano@gmail.com>
2018-03-08 18:10:27 -0600

    Create Concatenation.java

---
+++ src/engine/Concatenation.java
@@ -0,0 +1,21 @@
+package engine;
+
+import java.lang.StringBuilder;
+
+public class Concatenation
+{
+	public String concat(String baseStr, String concatStr)
+	{
+		return baseStr + concatStr;
+	}
+	
+	public String concatAtIndex(String baseStr, String concatStr, int index)
+	{		
+		StringBuilder sb = new StringBuilder();
+		sb.append(baseStr);
+		sb.insert(index, concatStr);
+		String strFinal = sb.toString();
+		
+		return strFinal;
+	}
+}
