3c1b31ba5f73f47579299c3562f846215e1f954c
Shtuffy <joshoplano@gmail.com>
2018-03-25 15:29:58 -0500

    Update SADSstring.java

--- src/engine/SADSstring.java
+++ src/engine/SADSstring.java
@@ -1,45 +1,24 @@
 package engine;
 
-/* Generates a string of 1s and 0s based on two rules, a starting string, and some length
-*  Example: rule 1 = 1 -> 10 and 0 -> 01, start 1, 3 times
-*  Result: 1101001
-*/
+import java.util.*;
 
 public class SADSstring 
 {
-	public static String Generate1(String rule1, String rule2, String startingString, int length)
-	{
-		String strFinal = startingString;
-	
-		for (int i = 0; i < length; i++) 
-		{	
-			if(strFinal.charAt(i) == '1')
-			{				
-				strFinal += rule1;
-			}
-			else 
-			{
-				strFinal += rule2;
-			}
-		}
-		return strFinal;
-	}
-	
-	public String Substitution(String startStr, String r1, String r2)
-	{
-		String strFinal = "";
-	
-		for (int i = 0; i < startStr.length(); i++)
+	public String Sub(String starting, LinkedHashMap<String, String> mappings)
+	{		
+		String finalResult = "";				
+		
+		for (int i = 0; i < starting.length(); i++)
 		{
-			if(startStr.charAt(i) == '0')
+			if(mappings.containsKey(Character.toString(starting.charAt(i))))
 			{
-				strFinal += r1;
+				finalResult += mappings.get(Character.toString(starting.charAt(i)));
 			}
 			else
 			{
-				strFinal += r2;
+				finalResult += starting.charAt(i);
 			}
-		}		
-		return strFinal;		
+		}						
+		return finalResult;
 	}
 }  
