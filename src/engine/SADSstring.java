3f3ed50016c58d3001f1cb50066a9054a1d7ad3a
Shtuffy <joshoplano@gmail.com>
2018-04-16 16:10:32 -0500

    Added iterations to substitute

--- src/engine/SADSstring.java
+++ src/engine/SADSstring.java
@@ -1,13 +1,22 @@
 package engine;
 
 import java.util.*;
+import gui.Console;
 
 public class SADSstring 
 {
 	public String Sub(String starting, LinkedHashMap<String, String> mappings)
 	{		
-		String finalResult = "";				
+		String finalResult = "";
 		
+		for(int i = 0; i < mappings.size(); i++)
+		{
+			if(!starting.contains(mappings.keySet().toArray()[i].toString()))
+			{
+				 Console.println(mappings.keySet().toArray()[i].toString() + " is not in the string.");
+			}
+		}
+
 		for (int i = 0; i < starting.length(); i++)
 		{
 			if(mappings.containsKey(Character.toString(starting.charAt(i))))
@@ -18,7 +27,32 @@ public class SADSstring
 			{
 				finalResult += starting.charAt(i);
 			}
-		}						
+		}		
+		return finalResult;
+	}
+	
+	public String Sub(String starting, LinkedHashMap<String, String> mappings, int iterations)
+	{		
+		String finalResult = starting;				
+		int UPPER = 100;
+		
+		if(iterations > UPPER)
+		{
+			UPPER = iterations;
+		}
+	
+		for(int i = 0; i < iterations; i++) 
+		{
+			String nextStep = Sub(finalResult, mappings);
+			if(nextStep.length() > UPPER - 1)
+			{
+				break;
+			}				
+			else
+			{
+				finalResult = nextStep;
+			}									
+		}
 		return finalResult;
 	}
 }  
